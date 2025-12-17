aabb8e6ad87615b2ac69072dcfde2102d71a9289
Llily Ella C <lilyellacallard@gmail.com>
2025-12-17 13:22:33 -0500

    adding solver.h

---
+++ solver.h
@@ -0,0 +1,4 @@
+#pragma once
+void hasLetter(char letter, std::string &word, bool &included);
+
+void greensRight(std::vector<char> &greens, bool &included, char &letter, std::string &word, bool &allIncluded, bool &rightSpot, std::vector<int> &greenPlacements);
